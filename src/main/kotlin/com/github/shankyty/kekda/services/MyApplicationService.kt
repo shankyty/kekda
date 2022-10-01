b3e1829a0b23d7da0e40d3ce19afd3fb9e93137f
GitHub Action <action@github.com>
2022-10-01 12:32:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/shankyty/kekda/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shankyty/kekda/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shankyty.kekda.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shankyty.kekda.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/shankyty/kekda/services/MyProjectService.kt
+++ src/main/kotlin/com/github/shankyty/kekda/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.shankyty.kekda.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shankyty.kekda.MyBundle
 
 class MyProjectService(project: Project) {
 
